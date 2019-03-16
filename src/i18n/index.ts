ec3f7c47faa52a85c1c1586340647bc222259dc0
lawvs <lawvs@users.noreply.github.com>
2019-03-16 18:32:22 +0800

    feat: i18n

---
+++ src/i18n/index.ts
@@ -0,0 +1,61 @@
+// https://www.i18next.com/
+import i18next from 'i18next'
+
+import en_US from './en-US.json'
+import ja_JP from './ja-JP.json'
+import zh_CN from './zh-CN.json'
+
+const DEBUG = process.env.NODE_ENV === 'development'
+
+const LOCALES = [
+  {
+    tag: 'zh-CN',
+    lng: '简体中文',
+    res: zh_CN,
+  },
+  {
+    tag: 'ja-JP',
+    lng: '日本語',
+    res: ja_JP,
+  },
+  {
+    tag: 'en-US',
+    lng: 'English',
+    res: en_US,
+  },
+]
+
+// LOCALES -> {'zh-CN': {translation: {...}}}
+const resources = LOCALES.reduce(
+  (acc, { tag, res }) => ({ ...acc, [tag]: { translation: res } }),
+  {},
+)
+
+const standardizeLocale = (language: string) => {
+  if (resources.hasOwnProperty(language)) {
+    return language
+  }
+  switch (language.substr(0, 2).toLowerCase()) {
+    case 'zh':
+      return 'zh-CN'
+    case 'ja':
+      return 'ja-JP'
+    default:
+      return 'en-US'
+  }
+}
+
+const DEFAULT_LANG = navigator.language
+const language = standardizeLocale(DEFAULT_LANG)
+
+i18next.init({
+  lng: language,
+  resources,
+  debug: DEBUG,
+  interpolation: {
+    escapeValue: false,
+  },
+})
+
+export { LOCALES as lngList }
+export default i18next
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import { render } from 'react-dom'
+// https://react.i18next.com/
+import { I18nextProvider } from 'react-i18next'
 
+import i18n from './i18n'
 import { GlobalStyle } from './styles'
 
 const App = () => <div>Hello World!</div>
@@ -8,7 +11,9 @@ const App = () => <div>Hello World!</div>
 render(
   <>
     <GlobalStyle />
-    <App />
+    <I18nextProvider i18n={i18n}>
+      <App />
+    </I18nextProvider>
   </>,
   document.querySelector('#root'),
 )
