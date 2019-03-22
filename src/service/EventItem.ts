32b29dfe7d1214fdca2476aa495eac041b613173
lawvs <lawvs@users.noreply.github.com>
2019-03-23 01:35:25 +0800

    fix: typo

--- src/service/EventItem.ts
+++ src/service/EventItem.ts
@@ -3,8 +3,8 @@ import { connect } from 'react-redux'
 import EventItems, { EventItemsProps } from '../components/EventItems'
 import { GlobalState } from '../store/types'
 
-const mapPropsToState = ({ mainState: { events } }: GlobalState): EventItemsProps => ({
+const mapStateToProps = ({ mainState: { events } }: GlobalState): EventItemsProps => ({
   events,
 })
 
-export default connect(mapPropsToState)(EventItems)
+export default connect(mapStateToProps)(EventItems)
