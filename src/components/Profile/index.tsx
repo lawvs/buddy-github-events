5693713a70d49b053edf4f5ae6af60c0216730b9
lawvs <lawvs@users.noreply.github.com>
2019-03-25 02:13:47 +0800

    feat: add profile

---
+++ src/components/Profile/index.tsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import styled from 'styled-components'
+
+import { GithubUser } from '../../api/types'
+import { spLayout } from '../Layout'
+
+const ProfileWrapper = styled.aside`
+  max-width: 230px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  border: 1px solid #d1d5da;
+  ${() => spLayout(`display: none;`)}
+
+  a {
+    color: #24292e;
+    text-decoration: none;
+    font-weight: 600;
+  }
+
+  a:hover {
+    text-decoration: underline;
+  }
+
+  p {
+    padding: 8px;
+    margin: 0;
+  }
+  h5 {
+    margin: 8px;
+  }
+`
+
+const Profile = (profileInfo?: GithubUser) =>
+  profileInfo ? (
+    <ProfileWrapper>
+      <img src={profileInfo.avatar_url} width={'100%'} />
+      <h5>
+        <a href={profileInfo.html_url} target="_blank" rel="noopener noreferrer">
+          {profileInfo.name ? profileInfo.name : profileInfo.login}
+        </a>
+      </h5>
+      <p>{profileInfo.bio}</p>
+    </ProfileWrapper>
+  ) : null
+
+export default Profile
---
+++ src/service/Profile.ts
@@ -0,0 +1,8 @@
+import { connect } from 'react-redux'
+
+import { GlobalState } from '../store/types'
+import Profile from '../components/Profile'
+
+const mapStateToProps = ({ mainState: { profileInfo } }: GlobalState) => profileInfo
+
+export default connect(mapStateToProps)(Profile)
