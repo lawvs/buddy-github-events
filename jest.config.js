786071e527a8601a06fc5e7d4e644290c1cd7981
lawvs <lawvs@users.noreply.github.com>
2019-04-10 00:19:07 +0800

    chore: test setup
    thanks for KagamiChan/raphtalia@5a47ac10e1dd3fa6b91

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  preset: 'ts-jest',
+  clearMocks: true,
+  coverageDirectory: 'coverage',
+  moduleFileExtensions: ['js', 'json', 'ts', 'tsx', 'node'],
+  testMatch: ['**/__tests__/**/*.{ts,tsx}', '**/?(*.)+(spec|test).{ts,tsx}'],
+  setupFilesAfterEnv: ['./src/setupTests.ts'],
+  collectCoverageFrom: ['./src/**/*.{ts,tsx}'],
+}
