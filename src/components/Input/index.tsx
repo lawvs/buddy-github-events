615f23d03bd00a498694eaf6343ab863e152c599
lawvs <lawvs@users.noreply.github.com>
2019-04-12 00:24:16 +0800

    refactor: input group

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,65 +1,9 @@
 import React from 'react'
-import styled from 'styled-components'
 import { withTranslation, WithTranslation } from 'react-i18next'
 
 import { GithubEventsType } from '../../api'
 import { GithubUser } from '../../api/types'
-import Tag from './Tag'
-
-const InputWrapper = styled.div<{ round?: boolean }>`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  height: 30px;
-  min-width: 300px;
-  padding: 0 12px;
-  margin: 8px;
-
-  background-color: hsla(0, 0%, 100%, 0.5);
-  box-shadow: 0 0 0 0 rgba(19, 124, 189, 0), 0 0 0 0 rgba(19, 124, 189, 0),
-    inset 0 0 0 1px rgba(16, 22, 26, 0.15), inset 0 1px 1px rgba(16, 22, 26, 0.2);
-
-  &:focus-within {
-    background-color: hsla(0, 0%, 100%, 1);
-  }
-
-  ${({ round = false }) => `
-    border-radius: ${round ? '30px' : '6px'};
-  `}
-
-  input {
-    flex: 1;
-    font-size: 16px;
-    outline: none;
-    border: none;
-    background-color: rgba(0, 0, 0, 0);
-  }
-`
-
-interface SearchButtonProps {
-  round?: boolean
-}
-
-const SearchButton = styled.div<SearchButtonProps>`
-  display: inline-flex;
-  justify-content: center;
-  align-items: center;
-  height: 30px;
-  min-width: 70px;
-  margin: 8px;
-  white-space: nowrap;
-  cursor: pointer;
-  user-select: none;
-  background: rgba(167, 182, 194, 0.3);
-  ${({ round = false }) => `
-    border-radius: ${round ? '30px' : '3px'};
-  `}
-
-  &:hover {
-    color: #182026;
-    background-color: #bfccd6;
-  }
-`
+import { InputWrapper, SearchButton, Tag, InputGroupWrapper } from './styles'
 
 export interface InputProps {
   username: string
@@ -108,7 +52,7 @@ const Input = ({
     )
 
   return (
-    <>
+    <InputGroupWrapper>
       <InputWrapper round>
         <input
           placeholder={t('Github username')}
@@ -128,7 +72,7 @@ const Input = ({
       <SearchButton round onClick={search}>
         {t('Search')}
       </SearchButton>
-    </>
+    </InputGroupWrapper>
   )
 }
 
---
+++ src/components/Input/styles.ts
@@ -0,0 +1,86 @@
+import styled from 'styled-components'
+
+export const InputGroupWrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+`
+
+export const InputWrapper = styled.div<{ round?: boolean }>`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 30px;
+  min-width: 300px;
+  padding: 0 12px;
+  margin: 8px;
+
+  background-color: hsla(0, 0%, 100%, 0.5);
+  box-shadow: 0 0 0 0 rgba(19, 124, 189, 0), 0 0 0 0 rgba(19, 124, 189, 0),
+    inset 0 0 0 1px rgba(16, 22, 26, 0.15), inset 0 1px 1px rgba(16, 22, 26, 0.2);
+
+  &:focus-within {
+    background-color: hsla(0, 0%, 100%, 1);
+  }
+
+  ${({ round = false }) => `
+  border-radius: ${round ? '30px' : '6px'};
+`}
+
+  input {
+    flex: 1;
+    font-size: 16px;
+    outline: none;
+    border: none;
+    background-color: rgba(0, 0, 0, 0);
+  }
+`
+
+interface SearchButtonProps {
+  round?: boolean
+}
+
+export const SearchButton = styled.div<SearchButtonProps>`
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  height: 30px;
+  min-width: 70px;
+  margin: 8px;
+  white-space: nowrap;
+  cursor: pointer;
+  user-select: none;
+  background: rgba(167, 182, 194, 0.3);
+  ${({ round = false }) => `
+  border-radius: ${round ? '30px' : '3px'};
+`}
+
+  &:hover {
+    color: #182026;
+    background-color: #bfccd6;
+  }
+`
+
+interface TagProps {
+  round?: boolean
+}
+
+export const Tag = styled.span<TagProps>`
+  display: inline-flex;
+  flex-direction: row;
+  align-items: center;
+  padding: 2px 6px;
+  white-space: nowrap;
+  cursor: pointer;
+  user-select: none;
+  ${({ round = false }) => `
+    border-radius: ${round ? '30px' : '3px'};
+  `}
+
+  color: #ddd;
+  background-color: #182026;
+  &:hover {
+    color: #182026;
+    background-color: #bfccd6;
+  }
+`
